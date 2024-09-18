490fb50e94d3ac0bf28d04ad1fe69c986cd92561
Esculap ESA <93584578+esculapesa@users.noreply.github.com>
2024-09-18 13:29:43 +0200

    EsaCoin Token

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,4 @@
-const MyContract = artifacts.require("HetherToken");
+const MyContract = artifacts.require("EsaCoinToken");
 
 module.exports = function (deployer) {
     deployer.deploy(MyContract);
