869d8e95d17d8d20d0f7c602edd4f26b37ee82ee
Esculap ESA <93584578+esculapesa@users.noreply.github.com>
2024-10-28 16:56:21 +0100

    simplified initial supply

--- contracts/GoldToken.sol
+++ contracts/GoldToken.sol
@@ -6,6 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract GLDToken is ERC20 {
     constructor() ERC20("Gold", "GLD") {
-        _mint(msg.sender, web3.utils.toWei('210000000', 'ether'));
+        _mint(msg.sender, 210000000);
     }
 }
