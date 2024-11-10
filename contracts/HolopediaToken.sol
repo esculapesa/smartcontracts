4e2ecad89007364f89e325cbe290e81182dcc027
Esculap ESA <93584578+esculapesa@users.noreply.github.com>
2024-11-10 10:49:04 +0100

    import IERC20 from zeppelin in all tokens

--- contracts/HolopediaToken.sol
+++ contracts/HolopediaToken.sol
@@ -1,17 +1,7 @@
 	// SPDX-License-Identifier: MIT
 	pragma solidity ^0.8.0;
 
-	interface IERC20 {
-		function totalSupply() external view returns (uint256);
-		function balanceOf(address account) external view returns (uint256);
-		function transfer(address recipient, uint256 amount) external returns (bool);
-		function allowance(address owner, address spender) external view returns (uint256);
-		function approve(address spender, uint256 amount) external returns (bool);
-		function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-
-		event Transfer(address indexed from, address indexed to, uint256 value);
-		event Approval(address indexed owner, address indexed spender, uint256 value);
-	}
+	import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 	contract HolopediaToken is IERC20 {
 		string public constant name = "Holopedia";
