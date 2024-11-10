e508a185310d39150b32c21c8e24f2fcf9ac7124
Esculap ESA <93584578+esculapesa@users.noreply.github.com>
2024-11-10 11:09:06 +0100

    bulk token deploy

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,20 @@
-const contracts = ["HolonToken", "INTToken", "InfareToken", "EsculapToken", "EsaCoinToken", "HolopediaToken", "HetherToken", "HetherMEXToken"];
+const EsaCoinToken = artifacts.require("EsaCoinToken");
+
+const tokens = [
+    { name: "EsaCoin", symbol: "ESA", initialSupply: "210000000" },
+    { name: "Holon", symbol: "HNS", initialSupply: "210000000" },
+    { name: "Int", symbol: "INT", initialSupply: "210000000" },
+    { name: "Infare", symbol: "IFE", initialSupply: "210000000" },
+    { name: "Esculap", symbol: "ESA", initialSupply: "210000000" },
+    { name: "Holopedia", symbol: "HPA", initialSupply: "210000000" },
+    { name: "Hether", symbol: "HTR", initialSupply: "210000000" },
+    { name: "Hether MEX", symbol: "HTM", initialSupply: "210000000" }
+];
 
 module.exports = async function (deployer) {
-    for (const contractName of contracts) {
-        const Contract = artifacts.require(contractName);
-        await deployer.deploy(Contract);
+    for (const token of tokens) {
+        await deployer.deploy(EsaCoinToken, token.name, token.symbol, web3.utils.toWei(token.initialSupply, "ether"));
+        const deployedToken = await EsaCoinToken.deployed();
+        console.log(`Deployed ${token.name} with symbol ${token.symbol} and supply ${token.initialSupply}`);
     }
 };
