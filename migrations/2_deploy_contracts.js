0838fc5716081d778fba98b38d1d681e1d22e1f1
Esculap ESA <93584578+esculapesa@users.noreply.github.com>
2024-11-10 11:15:46 +0100

    token sympol and order corrected

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,11 @@
 const EsaCoinToken = artifacts.require("EsaCoinToken");
 
 const tokens = [
-    { name: "EsaCoin", symbol: "ESA", initialSupply: "210000000" },
     { name: "Holon", symbol: "HNS", initialSupply: "210000000" },
     { name: "Int", symbol: "INT", initialSupply: "210000000" },
     { name: "Infare", symbol: "IFE", initialSupply: "210000000" },
     { name: "Esculap", symbol: "ESA", initialSupply: "210000000" },
+    { name: "EsaCoin", symbol: "ESC", initialSupply: "210000000" },
     { name: "Holopedia", symbol: "HPA", initialSupply: "210000000" },
     { name: "Hether", symbol: "HTR", initialSupply: "210000000" },
     { name: "Hether MEX", symbol: "HTM", initialSupply: "210000000" }
