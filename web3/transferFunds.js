b45335663dab4227b0200483910111ae4459d3fc
Esculap ESA <93584578+esculapesa@users.noreply.github.com>
2024-11-18 20:19:14 +0100

    transfer remaining 20% from 90% due

--- web3/transferFunds.js
+++ web3/transferFunds.js
@@ -8,14 +8,14 @@ const privateKey = process.env.copperkey;
 
 // List of tokens and amounts to transfer individually
 const tokens = [
-    { address: "0xcDbBC3fC0466f35D102441E2216A5888A54Cb372", amount: web3.utils.toWei("147000000", "ether") },  // EsaCoin (ESC)
-    { address: "0xaa9cFb915654772e971D540292152F20cd25B46b", amount: web3.utils.toWei("147000000", "ether") },  // Esculap (ESA)
-    { address: "0x8CB4c1B4094e58Ff8a071421c7d1cf87daA1BCDe", amount: web3.utils.toWei("147000000", "ether") },  // Hether (HTR)
-    { address: "0x7fCE524C610acE8b694a3Ef615581BcA831544cb", amount: web3.utils.toWei("147000000", "ether") },  // Hether MEX (HTM)
-    { address: "0x7e6D75B1A8Bd04778387DFb7063D192F835D084e", amount: web3.utils.toWei("147000000", "ether") },  // Holon (HNS)
-    { address: "0xA27e4e424a5eF14b03A7ECA81a6cFB496dbD3740", amount: web3.utils.toWei("147000000", "ether") },  // Holopedia (HPA)
-    { address: "0x3f65642A1621466E0B467692839D87237435794C", amount: web3.utils.toWei("147000000", "ether") },  // Infare (IFE)
-    { address: "0x664BEb8E762B19346d34C8A4c02705662371d5d1", amount: web3.utils.toWei("147000000", "ether") }   // Int (INT)
+    { address: "0xcDbBC3fC0466f35D102441E2216A5888A54Cb372", amount: web3.utils.toWei("42000000", "ether") },  // EsaCoin (ESC)
+    { address: "0xaa9cFb915654772e971D540292152F20cd25B46b", amount: web3.utils.toWei("42000000", "ether") },  // Esculap (ESA)
+    { address: "0x8CB4c1B4094e58Ff8a071421c7d1cf87daA1BCDe", amount: web3.utils.toWei("42000000", "ether") },  // Hether (HTR)
+    { address: "0x7fCE524C610acE8b694a3Ef615581BcA831544cb", amount: web3.utils.toWei("42000000", "ether") },  // Hether MEX (HTM)
+    { address: "0x7e6D75B1A8Bd04778387DFb7063D192F835D084e", amount: web3.utils.toWei("42000000", "ether") },  // Holon (HNS)
+    { address: "0xA27e4e424a5eF14b03A7ECA81a6cFB496dbD3740", amount: web3.utils.toWei("42000000", "ether") },  // Holopedia (HPA)
+    { address: "0x3f65642A1621466E0B467692839D87237435794C", amount: web3.utils.toWei("42000000", "ether") },  // Infare (IFE)
+    { address: "0x664BEb8E762B19346d34C8A4c02705662371d5d1", amount: web3.utils.toWei("42000000", "ether") }   // Int (INT)
 ];
 
 const tokenAbi = [
