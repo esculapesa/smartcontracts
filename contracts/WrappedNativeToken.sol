8d42d8283d3b1d39acd92a7a33a91cf711715aaf
Esculap ESA <93584578+esculapesa@users.noreply.github.com>
2024-11-24 20:57:15 +0100

    security to utils

--- contracts/WrappedNativeToken.sol
+++ contracts/WrappedNativeToken.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 contract WrappedNativeToken is ERC20, ReentrancyGuard {
     constructor() ERC20("Wrapped ESA", "WESA") {}
