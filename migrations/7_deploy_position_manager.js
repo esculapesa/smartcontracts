95e9e22819397660f9f070d7a32d655f2e352630
Esculap ESA <93584578+esculapesa@users.noreply.github.com>
2024-11-24 23:52:24 +0100

    sol from npm

--- migrations/7_deploy_position_manager.js
+++ migrations/7_deploy_position_manager.js
@@ -1,11 +1,14 @@
 // This script will deploy the Nonfungible Position Manager contract using Truffle
 
-const NonfungiblePositionManager = artifacts.require("NonfungiblePositionManager");
+const path = require('path');
+const NonfungiblePositionManagerArtifact = require('@uniswap/v3-periphery/artifacts/contracts/NonfungiblePositionManager.sol/NonfungiblePositionManager.json');
+
+const NonfungiblePositionManager = artifacts.require(path.resolve(__dirname, '../node_modules/@uniswap/v3-periphery/artifacts/contracts/NonfungiblePositionManager.sol/NonfungiblePositionManager.json'));
 
 module.exports = function (deployer) {
   deployer.deploy(NonfungiblePositionManager, 
     '0xF0f274EA0ad60FA7d75490f0Da58fF710ADea475',   // Uniswap V3 Factory Address on your private network
-    '0xe2C8bE486A82740406986Fc5Bd696e0A02cb852C'       // WESA Token Address on your private network
+    '0xe2C8bE486A82740406986Fc5Bd696e0A02cb852C'    // WESA Token Address on your private network
   )
     .then(() => {
       console.log("Nonfungible Position Manager deployed to:", NonfungiblePositionManager.address);
